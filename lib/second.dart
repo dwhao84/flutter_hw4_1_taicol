e9d97ca8d5bf470e817db0adafa272f6bea6f946
Dawei Hao <dwsamurai84@gmail.com>
2024-12-21 21:27:11 +0800

    refactor the second.dart

--- lib/second.dart
+++ lib/second.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-
+import 'detailPage.dart';
 import 'taiwan_endemic_species.dart';
 
 class SecondPage extends StatelessWidget {
@@ -72,65 +72,6 @@ class SpeciesList extends StatelessWidget {
           },
         );
       },
-    );
-  }
-}
-
-class DetailPage extends StatelessWidget {
-  final TaiwanEndemicSpecies species;
-
-  const DetailPage({
-    Key? key,
-    required this.species,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 21, 55, 24),
-        title: Text(
-          species.name,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              species.image,
-              width: double.infinity,
-              height: 250,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    species.name,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    species.description,
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
