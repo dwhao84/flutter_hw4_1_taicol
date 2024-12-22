40fe4b884b8fef671b1fd85833c1c72eabcc564d
Dawei Hao <dwsamurai84@gmail.com>
2024-12-22 23:02:30 +0800

    create my_name.dart

---
+++ lib/my_name.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyName extends StatelessWidget {
+  const MyName({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Dawei',
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
